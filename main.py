2d6fa183bd4136b61bd73d1dad24f6e1a1b4b1bf
ryry <ryry2nd@gmail.com>
2022-06-04 17:01:55 -0400

    optimisation

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 #imports
-from flask import Flask, render_template, Response
-import cv2
+from flask import Flask, Response
+import cv2, time
 
 #init vars
+FPS = 30
 camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FPS, 10)
-fps = int(camera.get(5))
 app = Flask('app')
 
 #generates the camera frames
 def gen_frames():  
+    lastFrameTime = time.time()
     while True:
         success, frame = camera.read()  # read the camera frame
         if success:
-            ret, buffer = cv2.imencode('.jpg', cv2.resize(frame, (16, 16)))
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-            
-            k = cv2.waitKey(1)
+            ret, buffer = cv2.imencode('.jpg', cv2.resize(frame, (64, 64)))
+            yield(b'--frame\r\n'b'Content-Type:image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+            sleepTime = 1./FPS - (time.time() - lastFrameTime)
+            if sleepTime > 0:
+                time.sleep(sleepTime)
+            lastFrameTime = time.time()
 
 #the video feed
-@app.route('/video_feed')
+@app.route('/')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-#root
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 #main function
 def main():
